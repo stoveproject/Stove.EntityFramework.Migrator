fb414a009223a4569e695d2a2ffe2d2c85fa1c58
osoykan <oguzhansoykan@gmail.com>
2017-03-12 20:03:28 +0300

    refactor attribute

--- Domain.Data.FluentMigrator/KnownMigrationAttribute.cs
+++ Domain.Data.FluentMigrator/KnownMigrationAttribute.cs
@@ -6,22 +6,22 @@ namespace Domain.Data.FluentMigrator
 {
     public class KnownMigrationAttribute : MigrationAttribute
     {
-        public KnownMigrationAttribute(string author, string jiraIssueOrBranchName, Type migrationType, int year, int month, int day, int hour, int minute)
-            : base(CalculateValue(jiraIssueOrBranchName.GetHashCode(), year, month, day, hour, minute), GenerateDescription(author, migrationType))
+        public KnownMigrationAttribute(string author, string issueName, Type migrationType, int year, int month, int day, int hour, int minute)
+            : base(CalculateValue(issueName.GetHashCode(), year, month, day, hour, minute), GenerateDescription(author, issueName, migrationType))
         {
             Author = author;
         }
 
         public string Author { get; private set; }
 
-        private static string GenerateDescription(string author, Type migrationType)
+        private static string GenerateDescription(string author, string issueName, Type migrationType)
         {
-            return $"Author:{author}, Migration:{migrationType.Name}";
+            return $"Author:{author}, Issue:{issueName}, Migration:{migrationType.Name}";
         }
 
-        private static long CalculateValue(int jiraIssueOrBranchNameHash, int year, int month, int day, int hour, int minute)
+        private static long CalculateValue(int issueName, int year, int month, int day, int hour, int minute)
         {
-            return jiraIssueOrBranchNameHash * 1000000000000L + year * 100000000L + month * 1000000L + day * 10000L + hour * 100L + minute;
+            return issueName * 1000000000000L + year * 100000000L + month * 1000000L + day * 10000L + hour * 100L + minute;
         }
     }
 }
