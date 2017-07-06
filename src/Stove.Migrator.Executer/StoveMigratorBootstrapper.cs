e99f1f95cfe2f303de44acc1e1dc88cfa8493f71
Ayberk Cantürk <ayberk.canturk@trendyol.com>
2017-07-06 10:39:57 +0300

    UnitOfWork transaction scope timeout #8

--- src/Stove.Migrator.Executer/StoveMigratorBootstrapper.cs
+++ src/Stove.Migrator.Executer/StoveMigratorBootstrapper.cs
@@ -12,7 +12,7 @@ namespace Stove.Migrator.Executer
     {
         public override void PreStart()
         {
-            Configuration.GetConfigurerIfExists<IStoveVersionInfoConfiguration>()(Resolver.Resolve<IStoveVersionInfoConfiguration>());
+            Configuration.GetConfigurerIfExists<IStoveMigrationConfiguration>()(Resolver.Resolve<IStoveMigrationConfiguration>());
         }
 
         public override void Shutdown()
--- src/Stove.Migrator/Versioning/StoveStoveMigrationConfiguration.cs
+++ src/Stove.Migrator/Versioning/StoveStoveMigrationConfiguration.cs
@@ -2,10 +2,12 @@
 
 namespace Stove.Versioning
 {
-    public class StoveStoveVersionInfoConfiguration : IStoveVersionInfoConfiguration, ISingletonDependency
+    public class StoveStoveMigrationConfiguration : IStoveMigrationConfiguration, ISingletonDependency
     {
         public string Schema { get; set; }
 
         public string Table { get; set; }
+
+        public int TransactionTimeout { get; set; }
     }
 }
